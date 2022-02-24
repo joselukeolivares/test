a434d20db0815765429e8e6ac02f9ff26c6dfb56
jose luis olivares <joselukeolivares@gmail.com>
2022-02-24 10:10:37 -0700

    updated to hello world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello test</h1>
+        <h1>Hello world</h1>
       </header>
     </div>
   );
