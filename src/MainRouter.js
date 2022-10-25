f6204939d995d0e629a6a0295c18f8d74a083d42
Jose Luis Olivares Camacho <joselukeolivares@gmail.com>
2022-10-25 13:16:58 -0500

    Rollback to folder CoppelGrowthup

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -20,7 +20,6 @@ import {HeatMapChartDC} from "./components/HeatMap"
 import { Sankey } from "./components/Sankey";
 import {DashboardBI} from "./components/DasboardBI"
 import {RendicionIMG} from "./components/RendicionIMG"
-import { CoppelGrowthMap } from "./components/CoppelGrowth";
 
 const MainRouter=()=>{
 
@@ -47,7 +46,7 @@ const MainRouter=()=>{
             <Route path="/test/d3/proyectosCoppel" element={<Sankey/>}/>
             <Route path="/test/dasboard_bi" element={<DashboardBI/>} />
             <Route path="/test/rendicionimg" element={<RendicionIMG/>} />
-            <Route path="/test/d3/coppelMap" element={<CoppelGrowthMap/>} />
+            
               
                
             </Routes>
